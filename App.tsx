ff9695cecda6ffaf9c7731a3c905f3a989cba3ac
NoorKassim <norkassem8@gmail.com>
2023-05-31 18:44:14 +0300

    ..

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import react, { cloneElement, useEffect, useState } from "react";
-import { Button, Image, ImageBackground,ScrollView,StyleSheet,Text,TextInput,View } from "react-native";
+import react, { cloneElement, useEffect, useState, } from "react";
+import { Button, Image, ImageBackground, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 import { Colors } from "react-native/Libraries/NewAppScreen";
 import Card from "./src /components/cards";
 import { get } from "react-native/Libraries/TurboModule/TurboModuleRegistry";
@@ -8,30 +8,30 @@ const App = () => {
   const [x, setX] = useState(0)
   const [txt, setTxt] = useState("")
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log("did load");
-    
 
-    return ()=> {
-      console.log("component will unmount"); 
+
+    return () => {
+      console.log("component will unmount");
     }
-  } , [])
+  }, [])
 
   useEffect(() => {
-      console.log("text changed, the new text is: " , txt);
-  } , [txt])
+    console.log("text changed, the new text is: ", txt);
+  }, [txt])
 
   return (
     <View>
       <Text style={style.text}>{txt}</Text>
-      <TextInput 
-      style={style.text}
-      value={txt} 
-      onChangeText={(val)=> setTxt(val)}
-      placeholder='text'
-       />
+      <TextInput
+        style={style.text}
+        value={txt}
+        onChangeText={(val) => setTxt(val)} 
+        placeholder='text'
+      />
 
-      <Button title="press" onPress={()=> setTxt("")}/>
+      <Button title="press" onPress={() => setTxt("")} />
 
     </View>
   )
@@ -43,76 +43,76 @@ const style = StyleSheet.create({
   }
 })
 
-    const data = [
-      {
-        name: 'brahem',
-        bd: '19.3', 
-        bgc:'red',
-      
-        
-      },
-      {
-        name: 'saleh',
-        bd: '23.6',
-        bgc:'blue'
-      },
-      {
-        name: 'huda',
-        bd: '21.10',
-      },
-      {
-        name: 'nor',
-        bd: '28.8'
-      } , 
-      {
-        name:'zeen' , 
-        bd:'24.8' , 
-      }, 
-      {
-        name:'rabea' , 
-        bd:'6.7',
-        
-      
-      }
+const data = [
+  {
+    name: 'brahem',
+    bd: '19.3',
+    bgc: 'red',
 
-      
-        
-      
-    ]
-  
-    const renderCards = ()=> {
-      return data.map(cardData => {
-        return <Card name={cardData.name} bd={cardData.bd} bgc={cardData.bgc}   />
-      })
-    
-  
-    return (
-      <View style={styles.container} >
-        <ScrollView showsVerticalScrollIndicator={false}>
-            {renderCards()}
-        </ScrollView>
-      </View>
-    );
-  
-    
-    }
-    
 
-  
-  
-  
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: 'white',
-      // justifyContent: 'center',
-      // alignItems: 'center' , 
-      paddingTop: 50
-    },
-  
-   
-   
+  },
+  {
+    name: 'saleh',
+    bd: '23.6',
+    bgc: 'blue'
+  },
+  {
+    name: 'huda',
+    bd: '21.10',
+  },
+  {
+    name: 'nor',
+    bd: '28.8'
+  },
+  {
+    name: 'zeen',
+    bd: '24.8',
+  },
+  {
+    name: 'rabea',
+    bd: '6.7',
+
+
+  }
+
+
+
+
+]
+
+const renderCards = () => {
+  return data.map(cardData => {
+    return <Card name={cardData.name} bd={cardData.bd} bgc={cardData.bgc} />
+  })
+
+
+  return (
+    <View style={styles.container} >
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {renderCards()}
+      </ScrollView>
+    </View>
+  );
+
+
+}
+
+
+
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    // justifyContent: 'center',
+    // alignItems: 'center' , 
+    paddingTop: 50
+  },
+
+
+
 
 
 })
@@ -125,7 +125,7 @@ const style = StyleSheet.create({
 
 
 
-  
+
 
 
 
@@ -159,7 +159,7 @@ export default App;
 
 
 
-  
+
 
 
 
